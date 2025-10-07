83e4b35753debca97e62b03fb685abc7109a39dc
beemstec <beemstec@gmail.com>
2025-10-07 11:39:44 +0530

    db creation

--- scripts/db_creation.sql
+++ scripts/db_creation.sql
@@ -1,3 +1,6 @@
-drop database snow_cli_cicd_demo_db;
-CREATE DATABASE snow_cli_cicd_demo_dev_db;
-CREATE schema snow_cli_cicd_demo_dev_db_sch;
+CREATE DATABASE snow_cli_cicd_demo_uat_db;
+CREATE schema snow_cli_cicd_demo_uat_db_sch;
+
+
+SELECT CURRENT_DATABASE();  -- Returns the current database name
+SELECT CURRENT_SCHEMA();    -- Returns the current schema name
